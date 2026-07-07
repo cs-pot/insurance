84e19f6aadcf98e8510cbb1bac577eb8fb956b7b
SKPG-Tech <42890752+SKPG-Tech@users.noreply.github.com>
2026-07-07 14:33:27 +0300

    SCRUM-29: Add integration tests
    Includes a basic entity creation and update test for the DB, and a single test hello world endpoint for the backend

--- insurahub-implementation/src/test/java/com/cspot/insurahub/entity/TestEntity.java
+++ insurahub-implementation/src/test/java/com/cspot/insurahub/entity/TestEntity.java
@@ -7,21 +7,26 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
 @Table(name = "test_table")
 public class TestEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
     private Long id;
 
+    @NonNull
     @Column(nullable = false)
     private String name;
-
-    protected TestEntity() {
-    }
-
-    public TestEntity(String name) {
-        this.name = name;
-    }
 }
