e6eca518895ac896b2546ae8882126cc7c3b153c
Gleb Kasachou <gleb.kasachou@gmail.com>
2026-07-09 20:22:11 +0300

    Remove ConsumerService interface

--- insurahub-implementation/src/main/java/com/cspot/insurahub/consumer/ConsumerService.java
+++ insurahub-implementation/src/main/java/com/cspot/insurahub/consumer/ConsumerService.java
@@ -1,9 +1,68 @@
 package com.cspot.insurahub.consumer;
 
+import com.cspot.insurahub.consumer.converter.ConsumerMapper;
+import com.cspot.insurahub.consumer.exception.EmailAlreadyInUseException;
+import com.cspot.insurahub.consumer.exception.IdentityProviderConflictException;
+import com.cspot.insurahub.consumer.exception.IdentityProviderRoleAssignmentException;
+import com.cspot.insurahub.consumer.exception.UserCreationException;
 import com.cspot.insurahub.model.ConsumerCreateRequest;
 import com.cspot.insurahub.model.ConsumerCreationResponse;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
 
-public interface ConsumerService {
+@Service
+@RequiredArgsConstructor
+public class ConsumerService {
 
-    ConsumerCreationResponse createConsumer(ConsumerCreateRequest consumerCreateRequest);
+    private final IdentityProviderClient identityProviderClient;
+    private final ConsumerRepository consumerRepository;
+    private final ConsumerMapper consumerMapper;
+
+    public ConsumerCreationResponse createConsumer(ConsumerCreateRequest consumerCreateRequest) {
+        try {
+            return attemptConsumerCreation(consumerCreateRequest);
+        } catch (DataIntegrityViolationException | IdentityProviderConflictException e) {
+            throw new EmailAlreadyInUseException(e);
+        } catch (Exception e) {
+            throw new UserCreationException(e);
+        }
+    }
+
+    private @NonNull ConsumerCreationResponse attemptConsumerCreation(ConsumerCreateRequest consumerCreateRequest) {
+        String idpId = null;
+        try {
+            idpId = registerUserWithIdp(consumerCreateRequest);
+            assignConsumerRoleToUser(idpId);
+            return persistConsumerData(consumerCreateRequest, idpId);
+        } catch (DataAccessException | IdentityProviderRoleAssignmentException e) {
+            deleteUserFromIdp(idpId);
+            throw e;
+        }
+    }
+
+    private void assignConsumerRoleToUser(String idpId) {
+        identityProviderClient.addUserRole(idpId, IdpRole.CONSUMER);
+    }
+
+    private void deleteUserFromIdp(String idpId) {
+        if (idpId != null) {
+            identityProviderClient.deleteUser(idpId);
+        }
+    }
+
+    private ConsumerCreationResponse persistConsumerData(ConsumerCreateRequest consumerCreateRequest,
+                                                         String idpId) {
+        Consumer consumer = consumerMapper.initializeFromCreateRequest(consumerCreateRequest);
+        consumer.setIdpId(idpId);
+        consumer = consumerRepository.save(consumer);
+        return new ConsumerCreationResponse(consumer.getId());
+    }
+
+    private String registerUserWithIdp(ConsumerCreateRequest consumerCreateRequest) {
+        return identityProviderClient.registerUser(consumerCreateRequest.getEmail(),
+                consumerCreateRequest.getPassword());
+    }
 }
