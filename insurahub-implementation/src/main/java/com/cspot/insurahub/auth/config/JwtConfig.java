c56df3ee8cb8a336507ea59d2c77f6f9f43d2d58
OJCodeCanvas <75716266+OJCodeCanvas@users.noreply.github.com>
2026-07-13 21:12:43 +0300

    fix: use DefaultJWTProcessor to forcefully accept at+jwt type

--- insurahub-implementation/src/main/java/com/cspot/insurahub/auth/config/JwtConfig.java
+++ insurahub-implementation/src/main/java/com/cspot/insurahub/auth/config/JwtConfig.java
@@ -1,7 +1,12 @@
 package com.cspot.insurahub.auth.config;
 
 import com.nimbusds.jose.JOSEObjectType;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.jwk.source.RemoteJWKSet;
 import com.nimbusds.jose.proc.DefaultJOSEObjectTypeVerifier;
+import com.nimbusds.jose.proc.JWSVerificationKeySelector;
+import com.nimbusds.jose.proc.SecurityContext;
+import com.nimbusds.jwt.proc.DefaultJWTProcessor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,25 +17,37 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtValidators;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
+import java.net.URL;
+
 @Configuration
 public class JwtConfig {
 
     @Bean
     public JwtDecoder jwtDecoder(
             @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}") String jwkSetUri,
-            BlacklistTokenValidator blacklistTokenValidator) {
+            @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}") String issuerUri,
+            BlacklistTokenValidator blacklistTokenValidator) throws Exception {
 
-        NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withJwkSetUri(jwkSetUri)
-                .jwtProcessorCustomizer(processor -> processor.setJWSTypeVerifier(
-                        new DefaultJOSEObjectTypeVerifier<>(JOSEObjectType.JWT, new JOSEObjectType("at+jwt"))))
-                .build();
+        // Use the raw processor to forcefully override the type verifier
+        DefaultJWTProcessor<SecurityContext> processor = new DefaultJWTProcessor<>();
+        processor.setJWSTypeVerifier(new DefaultJOSEObjectTypeVerifier<>(
+                JOSEObjectType.JWT, 
+                new JOSEObjectType("at+jwt")
+        ));
+        
+        // Set the JWK Source using the correct JWSVerificationKeySelector method
+        RemoteJWKSet<SecurityContext> jwkSource = new RemoteJWKSet<>(new URL(jwkSetUri));
+        processor.setJWSKeySelector(new JWSVerificationKeySelector<>(JWSAlgorithm.RS256, jwkSource));
 
-        OAuth2TokenValidator<Jwt> defaultValidator = JwtValidators.createDefault();
+        NimbusJwtDecoder decoder = new NimbusJwtDecoder(processor);
+
+        // Add default validation (issuer + expiry) and our blacklist validator
+        OAuth2TokenValidator<Jwt> defaultValidator = JwtValidators.createDefaultWithIssuer(issuerUri);
         OAuth2TokenValidator<Jwt> delegatingValidator = 
                 new DelegatingOAuth2TokenValidator<>(defaultValidator, blacklistTokenValidator);
 
-        jwtDecoder.setJwtValidator(delegatingValidator);
+        decoder.setJwtValidator(delegatingValidator);
 
-        return jwtDecoder;
+        return decoder;
     }
 }
