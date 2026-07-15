69d8089e2ef5a7d12e2ba59e0a21032909de3dda
Gleb Kasachou <gleb.kasachou@gmail.com>
2026-07-15 12:44:32 +0300

    Rename PostPackageRequest to PackageRequest

--- insurahub-implementation/src/main/java/com/cspot/insurahub/insurancepackage/controller/PackageController.java
+++ insurahub-implementation/src/main/java/com/cspot/insurahub/insurancepackage/controller/PackageController.java
@@ -3,7 +3,7 @@ package com.cspot.insurahub.insurancepackage.controller;
 import com.cspot.insurahub.api.PackagesApi;
 import com.cspot.insurahub.insurancepackage.service.PackageService;
 import com.cspot.insurahub.model.PostResponse;
-import com.cspot.insurahub.model.PostPackageRequest;
+import com.cspot.insurahub.model.PackageRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +18,7 @@ public class PackageController implements PackagesApi {
 
     @Override
     @PreAuthorize("hasAuthority('create:packages')")
-    public PostResponse postPackage(PostPackageRequest request) {
+    public PostResponse postPackage(PackageRequest request) {
         return packageService.createPackage(request);
     }
 
