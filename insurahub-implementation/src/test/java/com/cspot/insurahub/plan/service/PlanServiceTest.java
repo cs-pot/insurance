87dd014da3b18b805f7701946c3f72b8e2d80dc0
Gleb Kasachou <gleb.kasachou@gmail.com>
2026-07-24 10:38:01 +0300

    Fix tests

--- insurahub-implementation/src/test/java/com/cspot/insurahub/plan/service/PlanServiceTest.java
+++ insurahub-implementation/src/test/java/com/cspot/insurahub/plan/service/PlanServiceTest.java
@@ -29,6 +29,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
